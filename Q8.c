ecd19657c4c19974aef9864d61d984795e682c21
Kazan <andyswebking@gmail.com>
2021-10-24 21:06:29 -0700

    10/25 OJ Q8(defective)

---
+++ Q8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int recursive(int n);
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", recursive(n));
+
+    return 0;    
+}
+
+int recursive(int n) {
+    if (n == 1) {
+        return 2;
+    }
+    else if (n != 1) {
+        return recursive(n - 1) + recursive((int)floor((double)n / 2));
+    }
+}
